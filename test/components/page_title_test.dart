5706f214be6be54565ae0e977a382a1207887295
Enrique Marques Jr <enr@esaid.com.br>
2020-04-10 07:23:50 -0300

    Added Material App on PageTitle widget test

--- test/components/page_title_test.dart
+++ test/components/page_title_test.dart
@@ -1,10 +1,17 @@
 import 'package:checkthelists/components/page_title.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('title should be rendered', (WidgetTester tester) async {
     final widget = PageTitle(title: 'Test');
-    await tester.pumpWidget(widget);
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child:  widget
+        ),
+      ))
+    );
     expect(find.text('Test'), findsOneWidget);
   });
 }
